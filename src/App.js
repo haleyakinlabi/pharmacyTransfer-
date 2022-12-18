488cf7e2f90b2ddf1107f37949e739e0e6006b66
Nate Hopkins (hopsoft) <natehop@gmail.com>
2022-12-18 09:13:59 -0700

    Start iterating toward practical use of useEffect

--- src/App.js
+++ src/App.js
@@ -1,65 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import PharmacySelect from './components/PharmacySelect';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import PharmacySelect from './components/PharmacySelect'
 
-function App() {
+function App () {
+  const [currentPharmacy, setCurrentPharmacy] = useState()
+  const [newPharmacy, setNewPharmacy] = useState()
 
-//onChange function
+  useEffect(() => {
+    document.title = `${currentPharmacy} -> ${newPharmacy}`
+  })
 
-  const pharmacyChanged = (event) => {
-    debugger
+  const pharmacyChanged = event => {
+    const selectElement = event.target
+    const optionElements = selectElement.querySelectorAll('option')
+    const selectedIndex = selectElement.selectedIndex
+    const selectedOption = optionElements[selectedIndex]
+    const selectedValue = selectedOption.value
+
+    if (selectElement.id === 'current-pharmacy')
+      setCurrentPharmacy(selectedValue)
+
+    if (selectElement.id === 'new-pharmacy') setNewPharmacy(selectedValue)
   }
 
-
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Prescription Transfer Heber City</h1>
 
+      <p>The current pharmacy is {currentPharmacy}</p>
+      <p>The new pharmacy is {newPharmacy}</p>
+
       <form>
+        <PharmacySelect
+          id='current-pharmacy'
+          label='current pharmacy'
+          onPharmacyChanged={pharmacyChanged}
+        ></PharmacySelect>
 
-      <fieldset>
-          <label for="firstName">First Name:</label>
-          <input type="text" placeholder="John"></input>
-      </fieldset>
-
-      <fieldset>
-          <label for="lastName">Last Name:</label>
-          <input type="text" placeholder="Doe"></input>
-      </fieldset>
-        
-        <fieldset>
-          <label for="email">Email:</label>
-          <input type="email" placeholder="user@example.com"></input>
-        </fieldset>
-        
-        <fieldset>
-          <label for="prescriptions">Prescriptions:</label>
-          <textarea placeholder="medicine name(s)"> </textarea>
-        </fieldset>
-
-        <PharmacySelect label="current pharmacy" onChange={pharmacyChanged}></PharmacySelect>
-
-        <PharmacySelect label="new pharmacy"></PharmacySelect>
-
-        <fieldset>
-          <label for="startDate">Start Date:</label>
-          <input id="startDate" type="date"></input>
-        </fieldset>
-
-        <fieldset>
-          <label for="endDate">End Date:</label>
-          <input id="endDate" type="date"></input>
-        </fieldset>
-
-        <button>transfer my prescriptions</button>
-
+        <PharmacySelect
+          id='new-pharmacy'
+          label='new pharmacy'
+          onPharmacyChanged={pharmacyChanged}
+        ></PharmacySelect>
       </form>
-      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
 
 //Pharmacy App
 //1. List of Pharmacies (Heber)
@@ -69,3 +58,42 @@ export default App;
 //-length of time(start and end)
 //-list of medicine(text)
 
+/*
+
+        <fieldset>
+          <label for='firstName'>First Name:</label>
+          <input type='text' placeholder='John'></input>
+        </fieldset>
+
+        <fieldset>
+          <label for='lastName'>Last Name:</label>
+          <input type='text' placeholder='Doe'></input>
+        </fieldset>
+
+        <fieldset>
+          <label for='email'>Email:</label>
+          <input type='email' placeholder='user@example.com'></input>
+        </fieldset>
+
+        <fieldset>
+          <label for='prescriptions'>Prescriptions:</label>
+          <textarea placeholder='medicine name(s)'> </textarea>
+        </fieldset>
+
+        <PharmacySelect
+          label='current pharmacy'
+          onChange={pharmacyChanged}
+        ></PharmacySelect>
+
+        <PharmacySelect label='new pharmacy'></PharmacySelect>
+
+        <fieldset>
+          <label for='startDate'>Start Date:</label>
+          <input id='startDate' type='date'></input>
+        </fieldset>
+
+        <fieldset>
+          <label for='endDate'>End Date:</label>
+          <input id='endDate' type='date'></input>
+        </fieldset>
+*/
--- src/components/PharmacySelect.jsx
+++ src/components/PharmacySelect.jsx
@@ -1,28 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 const PharmacySelect = (props) => {
-
-    
-
-    
-
-    return(
-    
-        <fieldset>
-            <label for={props.id}>{props.label}</label>
-            <select id={props.id} onChange={props.onChange()}>
-                <option>Lees</option>
-                <option>Smiths</option>
-                <option>Walgreens</option>
-                <option>Walmart</option>
-            </select>
-        </fieldset>
-    )
+  return(
+    <fieldset>
+      <label for={props.id}>{props.label}</label>
+      <select id={props.id} onChange={props.onPharmacyChanged}>
+        <option>Lees</option>
+        <option>Smiths</option>
+        <option>Walgreens</option>
+        <option>Walmart</option>
+      </select>
+    </fieldset>
+  )
 }
-
-
-
-
 
 export default PharmacySelect
 
+// this is a component that behaves like a normal HTML <select> element
+// but that exposes some extra behavior
+// and is specialized for selecting pharmacies
+//
+// the <select> `change` event is native HTML DOM behavior
+// react exposes this as `onChange`
+//
+// this component expects a change event handler to be passed as a prop
